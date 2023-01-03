664b9c1c3195de53ad8570054da36d980a7dccde
Nikola Orlovski <nikikiko1@abv.bg>
2023-01-04 00:00:30 +0200

    New edits

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Application from "./js/Application";
 window.addEventListener ("DOMContentLoaded",  async () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
-  await app.load();
+  await app._load();
   // Used to access the app instance by the automated tests
   window.__JS_APP = app;
 });
